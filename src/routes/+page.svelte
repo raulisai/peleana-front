6794d056071cc5600c8d6fc17f0e22617eacb617
raulisai <raulisai97@gmail.com>
2023-11-18 17:10:54 -0600

    se usara tilwind, entonces se instalaron los paquetes y se hixo cambio a la pagina de inicio

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<div class="flex flex-col justify-center items-center h-screen  bg-gradient-to-r from-sky-500 to-indigo-500">
+
+<div>
+    <button class="bg-indigo-600 text-white text-lg leading-6 font-medium py-2 px-3 rounded-lg my-12">
+        <a href="/inicio">Inicio</a>
+    </button>
+</div>
+   <div>
+    <h2 class="text-xl text-yellow-200 lg:my-10">
+        Comienza a jugar el mejor juego de tablero
+       </h2>
+   </div>
+    
+   <div>
+    <span class="lg:text-4xl">Poleana</span>
+   </div>
+
+</div>
+
---
+++ src/routes/test/cell.svelte
@@ -0,0 +1,15 @@
+<!-- Cell.svelte -->
+<script>
+    export let value;
+  </script>
+  
+  <div class="cell border border-gray-300 w-16 h-16 flex items-center justify-center">
+    {#if value === 0}
+      <!-- Contenido específico cuando el valor es 0 -->
+      <span class="text-red-500">Contenido 0</span>
+    {:else}
+      <!-- Contenido para otros valores -->
+      <span>{value}</span>
+    {/if}
+  </div>
+  
